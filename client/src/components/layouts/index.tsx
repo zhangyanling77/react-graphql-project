84dc56315838e5ab03cddfd21cf3b6722f684605
zhangyanling77 <18783226594@163.com>
2019-12-30 14:27:23 +0800

    fix: 刷新后菜单与路由不对应的问题

--- client/src/components/layouts/index.tsx
+++ client/src/components/layouts/index.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout
 
-const Layouts: React.FC = (props) => (
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['1']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1"><Link to="/">商品管理</Link></Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: '50px 50px 0 50px' }}>
-      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-        {props.children}
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}> ©2019 Created by zhangyanling. </Footer>
-  </Layout>
-)
+const Layouts: React.FC = (props:any) => {
+  let [selectKey, setSelectKey] = useState<string>('/')
+  // console.log(props)
+  let path = props.location.pathname;
 
+  useEffect(()=> {
+    setSelectKey(path)
+  }, [path])
+  
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['/']}
+          selectedKeys={[selectKey]}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="/"><Link to="/">商品管理</Link></Menu.Item>
+          <Menu.Item key="/profile"><Link to="/profile">个人中心</Link></Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '50px 50px 0 50px' }}>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          {props.children}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}> ©2019 Created by zhangyanling. </Footer>
+    </Layout>
+  )  
+}
 
-export default Layouts;
+export default withRouter(memo(Layouts));
