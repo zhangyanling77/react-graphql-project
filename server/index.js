346e8176bdb09e842174f876ff1405e66b9f925d
zhangyanling77 <18783226594@163.com>
2020-01-06 17:25:10 +0800

    feat: add login logic

--- server/index.js
+++ server/index.js
@@ -1,24 +1,121 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
+const Router = require('koa-router');
 const cors = require('koa2-cors'); // 解决跨域
-const logger = require('koa-logger'); // 日志输出
+const logger = require('koa-logger');
+const bodyparser = require('koa-bodyparser');
+const jwt = require('jwt-simple');
+
 const myGraphQLSchema = require('./schema');
 
 const app = new Koa();
+const router = new Router();
+const PORT = 4000;
 
-app.use(logger())
+const secret = 'zyl';
+app.use(logger());
+
+app.use(bodyparser());
 
 app.use(cors({
-  origin: '*',
-  allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS']
-}))
+  origin: 'http://localhost:3000',
+  allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+  credentials: true // 允许跨域 携带cookie
+}));
 
-app.use(mount('/graphql', graphqlHTTP({
+
+// app.use(mount('/graphql', graphqlHTTP({
+//   schema: myGraphQLSchema,
+//   graphiql: true // 开启graphiql可视化操作ide
+// })));
+
+// 用户列表
+let userList = [
+  {
+    username: 'admin',
+    password: '123456'
+  },
+  {
+    username: 'guest',
+    password: '123456'
+  }
+];
+// 当前url
+let currentUrl = '';
+// 登录
+router.post('/login', async ctx => {
+  console.log("login~~~~", ctx.req.body)
+  // 登录接口
+  let {
+    username,
+    password
+  } = ctx.request.body;
+  let user = userList.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    let token = jwt.encode({
+      username,
+      url: currentUrl
+    }, secret);
+
+    ctx.body = {
+      code: 0,
+      data: {
+        username,
+        token,
+        url: currentUrl
+      }
+    }
+  } else {
+    ctx.body = {
+      code: 1,
+      data: '登录失败，请检查账号密码'
+    }
+  }
+});
+// 验证
+router.get('/validate', async (ctx) => {
+  let token = ctx.headers['token'];
+  if (!token) {
+    ctx.body = {
+      code: 1,
+      data: '用户未登录'
+    }
+  } else {
+    try {
+      let user = jwt.decode(token, secret);
+      console.log(user);
+      console.log(user, '----', {
+        user,
+        token,
+        url: currentUrl
+      });
+      ctx.body = {
+        code: 0,
+        data: {
+          ...user,
+          token,
+          url: currentUrl
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      ctx.body = {
+        code: 1,
+        data: 'token不正确'
+      }
+    }
+  }
+});
+
+router.all('/graphql', graphqlHTTP({
   schema: myGraphQLSchema,
-  graphiql: true // 开启graphiql可视化操作ide
-})))
+  graphiql: true
+}));
 
-app.listen(4000, () => {
-  console.log('server started on 4000')
-})
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`)
+});
