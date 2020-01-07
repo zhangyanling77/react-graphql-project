09a1ffd9a80c4126794b708d91916ad4bb4f2ef9
zhangyanling77 <18783226594@163.com>
2020-01-07 13:02:29 +0800

    Update config-overrides.js

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,11 +1,15 @@
-const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const { override, fixBabelImports, addWebpackAlias, addLessLoader } = require('customize-cra');
 const path = require('path')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    // style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true
   }),
   addWebpackAlias({
     "@": path.resolve(__dirname, "src/")              
