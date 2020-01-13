cee9e27cb0d203d631a0cc20aaf69c54ebe7b50a
zhangyanling77 <18783226594@163.com>
2020-01-13 09:25:21 +0800

    feat: add component

--- client/src/pages/productList/add/index.tsx
+++ client/src/pages/productList/add/index.tsx
@@ -1,1 +1,66 @@
-export {}
+import React, { memo, useState} from 'react';
+import { Modal, Form, Input, Select } from 'antd';
+import { Product, Category } from '@/types';
+import { useMutation } from '@apollo/react-hooks';
+import { GET_PRODUCTS, ADD_PRODUCT } from '@/api';
+
+const { Option } = Select;
+
+interface FormProps {
+  handleClose: () => void;
+  categories: Array<Category>;
+}
+
+const AddForm:React.FC<FormProps> = ({handleClose, categories}) => {
+  let [product, setProduct] = useState<Product>({ name: '', categoryId: [] });
+  let [addProduct] = useMutation(ADD_PRODUCT);
+
+  const handleSubmit = async () => {
+    await addProduct({
+      variables: product,
+      refetchQueries: [{
+        query: GET_PRODUCTS
+      }]
+    })
+    // 清空表单
+    setProduct({ name: '', categoryId: [] });
+    handleClose();
+  }
+  
+  return (
+    <Modal
+      title="新增商品"
+      visible={true}
+      onOk={handleSubmit}
+      okText="提交"
+      cancelText="取消"
+      onCancel={handleClose}
+      maskClosable={false}
+    >
+      <Form>
+        <Form.Item label="商品名称">
+          <Input 
+            placeholder="请输入" 
+            value={product.name} 
+            onChange={event => setProduct({ ...product, name: event.target.value })} 
+          />
+        </Form.Item>
+        <Form.Item label="商品分类">
+          <Select 
+            placeholder="请选择" 
+            value={product.categoryId} 
+            onChange={(value: string | []) => setProduct({ ...product, categoryId: value })}
+          >
+            {
+              categories.map((item: Category) => (
+                <Option key={item.id} value={item.id}>{item.name}</Option>
+              ))
+            }
+          </Select>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default memo(AddForm);
